8579684f7d7678387c990c5f982264a3c9593e25
qctran <18389502+qctran@users.noreply.github.com>
2020-03-02 20:15:06 -0800

    Initial checkin
    Add FindRepetitiveNumber project

---
+++ FindRepetitiveNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindRepetitiveNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new[] {0, 1, 2, 5, 2, 6, 2, 3, 9};
+            var dict = new Dictionary<int, int>();
+
+            for (var i = 0; i < a.Length; i++)
+            {
+                int value;
+                if (dict.ContainsKey(a[i]))
+                {
+                    dict.TryGetValue(a[i], out value);
+                    value++;
+                    dict[a[i]] = value;
+                }
+                else
+                {
+                    dict.Add(a[i], 1);
+                }
+            }
+
+            Console.WriteLine(dict.Values.Max());
+        }
+    }
+}

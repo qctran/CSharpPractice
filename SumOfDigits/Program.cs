80d92c05ddce438067e3c5d8c020692b6069cb07
qctran <18389502+qctran@users.noreply.github.com>
2020-03-02 20:20:34 -0800

    Initial checkin
    Add SumOfDigits project

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(123456789));
+        }
+
+        private static int Sum(int n)
+        {
+            if (n == 0)
+                return 0;
+            else
+            {
+                return n % 10 + Sum(n / 10);
+            }
+        }
+    }
+}

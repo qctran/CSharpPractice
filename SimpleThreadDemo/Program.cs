46b53f5dbd4f9c6de21dadbe9f5627d1258b49be
qctran <18389502+qctran@users.noreply.github.com>
2020-03-02 20:20:12 -0800

    Initial checkin
    Add SimpleThreadDemo project

---
+++ SimpleThreadDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleThreadDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var result = await LongWaitingTaskWrapper();
+            var result2 = await LongWaitingTask();
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+        }
+
+        private static async Task<int> LongWaitingTaskWrapper()
+        {
+            var result = await LongWaitingTask();
+            return result;
+        }
+        private static async Task<int> LongWaitingTask()
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);
+                if (i == 0)
+                {
+                    Console.WriteLine(i+1 + " second");
+                }
+                else
+                {
+                    Console.WriteLine(i + 1 + " seconds");
+                }
+            }
+            
+            return 1;
+        }
+    }
+}

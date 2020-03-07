afa0e9d652c211bf54d880f724c83f7b53e17791
qctran <18389502+qctran@users.noreply.github.com>
2020-03-06 16:44:52 -0800

    Initial checkin RemoveDuplicates project
    From Leetcode.com
    Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
    
    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Given a sorted array nums, remove the duplicates in-place such that each element 
+                appear only once and return the new length.
+
+                Do not allocate extra space for another array, you must do this by modifying the 
+                input array in-place with O(1) extra memory.
+
+                E.g.
+                Given nums = [1,1,2],
+                Your function should return length = 2, with the first two elements of nums being 
+                1 and 2 respectively.
+                It doesn't matter what you leave beyond the returned length.
+             */
+            var a = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            var newLen = RemoveDup(a);
+            Console.WriteLine("New length is " + newLen);
+            DisplayNewArray(a, newLen);
+        }
+
+        private static void DisplayNewArray(int[] a, int newLen)
+        {
+            Console.Write("[");
+            for(var i = 0; i < newLen; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine("]");
+        }
+
+        public static int RemoveDup(int[] nums)
+        {
+            if (nums.Length < 2) return nums.Length;
+
+            var i = 0;
+            
+            for(var j = 1; j < nums.Length; j++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                    
+                }
+                else
+                {
+                    
+                }
+            }
+
+            return i+1;
+        }
+
+        
+    }
+}

ec46c527ba94180a487de167211292475837532e
Conan Tran <conantran@outlook.com>
2020-09-06 19:02:29 -0700

    PrismWpfDemo ViewA & ViewB update

--- PrismWpfDemo/PrismWpfDemo/ViewModels/ViewBViewModel.cs
+++ PrismWpfDemo/PrismWpfDemo/ViewModels/ViewBViewModel.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Mvvm;
 
 namespace PrismWpfDemo.ViewModels
 {
-    public class ViewBViewModel
+    public class ViewBViewModel: BindableBase
     {
+        private string _myText = "My View B";
+        public string MyText
+        {
+            get { return _myText; }
+            set
+            {
+                SetProperty(ref _myText, value);
+            }
+        }
+
     }
 }

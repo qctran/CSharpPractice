17925f8a6948d8499156d723c080b750134c442a
Conan Tran <conantran@outlook.com>
2021-02-26 10:41:28 -0800

    Update TwoSum project with more test cases and handle when no sum found

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TwoSum
 {
@@ -10,12 +6,25 @@ namespace TwoSum
     {
         static void Main(string[] args)
         {
-            var testAry = new int[] { 12, 17, 7, 2 };
-            var result = TwoSum(testAry, 9);
-            if (result.Length > 1)
+            //var testAry = new int[] { 2, 7, 7, 2 };
+            //var testAry = new int[] { 2, 7 };
+            var testAry = new int[] { 1, 7, 7, 10, 9, 11, 2 };
+            //var testAry = new int[] { 12, 17, 7, 2 };
+            //var testAry = new int[] { 12, 17, 7, 1 };
+            var sumValue = 9;
+            var result = TwoSum(testAry, sumValue);
+
+            if (result == null)
             {
-                Console.WriteLine("[{0},{1}]", result[0], result[1]);
+                Console.WriteLine($"No pair of integer found to add up to {sumValue}");
             }
+            else if (result.Length > 1)
+            {
+                Console.WriteLine($"Values of the index [{result[0]}] and [{result[1]}] add up to {sumValue}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
 
         public static int[] TwoSum(int[] nums, int target)
@@ -33,7 +42,7 @@ namespace TwoSum
                     }
                 }
             }
-            return result;
+            return null;
         }
     }
 }

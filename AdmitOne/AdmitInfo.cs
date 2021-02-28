e446ac80a72ffda93f581d40ab5147627d4ed113
Conan Tran <conantran@outlook.com>
2021-02-27 16:30:46 -0800

    Add AdmitOne prj
    Requirements
    ● Implement the admitOne method of the Hospital interface. admitOne should print a log message with a timestamp and the name of the patient when that patient is finally admitted into the Hospital.
    ● Because there is only one receptionist, you can only admit one patient into the hospital every 5 seconds. For example, if admitOne is called three times (in parallel), it should take ~10 seconds for all three people to be admitted into the hospital. (patient 1 admitted at T-0, patient 2 admitted at T-5, and patient 3 admitted at T-10)
    ● Admittance to the hospital should be first-come-first-serve. In other words, patients should be admitted in the same order that admitOne was called in.
    ● Don't worry too much about multi-threading

---
+++ AdmitOne/AdmitInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace AdmitOne
+{
+    public class AdmitInfo
+    {
+        public string Name { get; set; }
+        public string DateTime { get; set; }
+    }
+}
---
+++ AdmitOne/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace AdmitOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdmit = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxOut = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAdmit
+            // 
+            this.btnAdmit.Location = new System.Drawing.Point(363, 48);
+            this.btnAdmit.Name = "btnAdmit";
+            this.btnAdmit.Size = new System.Drawing.Size(75, 23);
+            this.btnAdmit.TabIndex = 0;
+            this.btnAdmit.Text = "Admit";
+            this.btnAdmit.UseVisualStyleBackColor = true;
+            this.btnAdmit.Click += new System.EventHandler(this.btnAdmit_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(69, 48);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(269, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // listBoxOut
+            // 
+            this.listBoxOut.FormattingEnabled = true;
+            this.listBoxOut.Location = new System.Drawing.Point(15, 117);
+            this.listBoxOut.Name = "listBoxOut";
+            this.listBoxOut.Size = new System.Drawing.Size(773, 134);
+            this.listBoxOut.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxOut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.btnAdmit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdmit;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxOut;
+    }
+}
+

f29ae3a63661af2be4b2b8ae5d5fa884cfdc1680
Conan Tran <conantran@outlook.com>
2021-04-07 10:12:32 -0700

    Add unit tests for TemperatureReader project

---
+++ TemperatureReaderTests/CelsiusCalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using TemperatureReader;
+
+namespace TemperatureReaderTests
+{
+    class CelsiusCalculatorTests
+    {
+        [Test]
+        [TestCase((ushort)12288, 25, 0.00)]
+        [TestCase((ushort)65535, 350, 0.00)]
+        [TestCase((ushort)0, -50, 0.00)]
+        [TestCase((ushort)65280, 348.4, 0.00)]
+        [TestCase((ushort)255, -48.4, 0.00)]
+        [TestCase((ushort)48, -49.7, 0.00)]
+        public void GetDegree_ValidData_Pass(ushort data, double expectValue, double delta)
+        {
+            var calculator = new CelsiusCalculator();
+            var device = new TestDevice();
+            var output = new ConsoleDisplay();
+            var validator = new Validator(output);
+
+            var temp = new CelsiusCalculator();
+            var result = temp.GetDegree(data);
+            result = Math.Round(result, 1);
+
+            Assert.AreEqual(expectValue, result, delta);
+        }
+    }
+}
---
+++ TemperatureReaderTests/ValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TemperatureReader;
+
+namespace TemperatureReaderTests
+{
+    public class ValidatorTests
+    {
+        [Test]
+        [TestCase(new byte[] { 0xaa, 0x02, 0x30, 0x00, 0x55 })]
+        public void ValidateRawData_ValidData_Pass(byte[] data)
+        {
+            var output = new ConsoleDisplay();
+            var obj = new Validator(output);
+
+            var result = obj.ValidateRawData(data);
+
+            Assert.IsTrue(result);
+
+        }
+
+        [Test]
+        [TestCase(new byte[] { 0x33, 0x02, 0x30, 0x00, 0x55 })]
+        [TestCase(new byte[] { 0xaa, 0x02, 0x30, 0x00, 0xbb })]
+        [TestCase(new byte[] { 0xaa, 0x01, 0x30, 0x55 })]
+        public void ValidateRawData_InvalidData_Pass(byte[] data)
+        {
+            var output = new ConsoleDisplay();
+            var obj = new Validator(output);
+
+            var result = obj.ValidateRawData(data);
+
+            Assert.IsFalse(result);
+
+        }
+    }
+}
